d2d707a669f3f2cacb8d2190cd2f7c489678bae8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-30 20:53:53 +0300

    feat: Implement getAllSP function in CustomerController to retrieve all customers from the database

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,4 +1,14 @@
-const getAllSP = async (req, res) => {};
+const { getConnection } = require("../config/dbConfig");
+const getAllSP = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const sqlQuery = "CALL getAllCustomerSP ()";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
 const addDataSP = async (req, res) => {};
 const getByIdSP = async (req, res) => {};
 const updateByIdSP = async (req, res) => {};
