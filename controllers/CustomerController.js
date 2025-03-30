c2873a7ae59827187a0b76a77678b3fdfa96c72d
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-30 16:22:20 +0300

    feat: Implement CustomerController with CRUD operations for customer data

---
+++ controllers/CustomerController.js
@@ -0,0 +1,13 @@
+const getAllSP = async (req, res) => {};
+const addDataSP = async (req, res) => {};
+const getByIdSP = async (req, res) => {};
+const updateByIdSP = async (req, res) => {};
+const deleteByIdSP = async (req, res) => {};
+
+module.exports = {
+  getAllSP,
+  addDataSP,
+  getByIdSP,
+  updateByIdSP,
+  deleteByIdSP,
+};
