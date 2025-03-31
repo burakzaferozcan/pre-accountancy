2bb30613d35d396e9d7886f918c6b9a61d33734c
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 23:16:50 +0300

    feat: Add deletePaymentSP function in ProductPurchaseController for deleting payments by ID with input validation and error handling

--- controllers/ProductPurchaseController.js
+++ controllers/ProductPurchaseController.js
@@ -73,8 +73,28 @@ const addPaymentSP = async (req, res) => {
   }
 };
 
+const deletePaymentSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL deletePaymentByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   addProductPurchaseSP,
   deleteProductPurchaseSP,
   addPaymentSP,
+  deletePaymentSP,
 };
