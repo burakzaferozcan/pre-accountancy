e04df094290b4a1868b1101f549023f5caa75ec2
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:42:35 +0300

    feat: Add getByIdSP function in ServiceController to retrieve service details by ID with error handling and input validation

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -37,7 +37,27 @@ const addDataSP = async (req, res) => {
   }
 };
 
+const getByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL getServiceByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   getAllSP,
   addDataSP,
+  getByIdSP,
 };
