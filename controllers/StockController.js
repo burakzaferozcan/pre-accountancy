e7b4b4d36b30cd408ffa47a9e509986032eea56b
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:54:53 +0300

    feat: Add deleteByIdSP function in StockController to delete stock data by ID with input validation and error handling

--- controllers/StockController.js
+++ controllers/StockController.js
@@ -74,10 +74,29 @@ const updateByIdSP = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+const deleteByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL deleteStockByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
   getAllSP,
   addDataSP,
   getByIdSP,
   updateByIdSP,
+  deleteByIdSP,
 };
