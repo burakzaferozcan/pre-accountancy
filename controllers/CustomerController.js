74b8e6c5c9758a509c4311a0524e42c54e865e5a
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:33:47 +0300

    feat: Implement updateByIdSP function in CustomerController to update customer details with input validation and error handling

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -18,7 +18,7 @@ const addDataSP = async (req, res) => {
     const sPhone = sanityFunction(phone);
     const sAddress = sanityFunction(address);
     const sReference = sanityFunction(reference);
-    if (!sFullName || !sPhone || !sAddress || !sTckn || !sReference) {
+    if (!sFullName || !sTckn || !sPhone || !sAddress || !sReference) {
       res.send({
         success: false,
         message: "Lütfen geçerli veriler ile tekrar deneyin.",
@@ -28,9 +28,9 @@ const addDataSP = async (req, res) => {
     const sqlQuery = "CALL addCustomerSP (?,?,?,?,?)";
     const response = await connection.query(sqlQuery, [
       sFullName,
+      sTckn,
       sPhone,
       sAddress,
-      sTckn,
       sReference,
     ]);
     res.send(response[0][0].result);
@@ -57,7 +57,38 @@ const getByIdSP = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
-const updateByIdSP = async (req, res) => {};
+const updateByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let { fullName, tckn, phone, address, reference } = req.body;
+    const sId = sanityFunction(id);
+
+    const sFullName = sanityFunction(fullName);
+    const sTckn = sanityFunction(tckn);
+    const sPhone = sanityFunction(phone);
+    const sAddress = sanityFunction(address);
+    const sReference = sanityFunction(reference);
+    if ((!sId, !sFullName || !sTckn || !sPhone || !sAddress || !sReference)) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL updateCustomerByIdSP (?,?,?,?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sId,
+      sFullName,
+      sTckn,
+      sPhone,
+      sAddress,
+      sReference,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 const deleteByIdSP = async (req, res) => {};
 
 module.exports = {
