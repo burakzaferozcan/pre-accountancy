f9c9b95a929c05b11a80289f703774d730a3ea77
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:34:59 +0300

    feat: Implement deleteByIdSP function in CustomerController to delete a customer by ID with input validation and error handling

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -89,7 +89,24 @@ const updateByIdSP = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
-const deleteByIdSP = async (req, res) => {};
+const deleteByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL deleteCustomerByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
   getAllSP,
