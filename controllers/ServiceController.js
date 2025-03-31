3ba75025b0c9ff7d1fea6482ce1ec3903fcc6fa3
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:41:27 +0300

    feat: Add addDataSP function in ServiceController to insert new service data with input validation using sanityFunction

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -1,5 +1,5 @@
 const { getConnection } = require("../config/dbConfig");
-
+const { sanityFunction } = require("../utils/sanityFunction");
 const getAllSP = async (req, res) => {
   try {
     const connection = await getConnection();
@@ -10,7 +10,34 @@ const getAllSP = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+const addDataSP = async (req, res) => {
+  try {
+    let { fullName, tckn, phone, address } = req.body;
+    const sFullName = sanityFunction(fullName);
+    const sTckn = sanityFunction(tckn);
+    const sPhone = sanityFunction(phone);
+    const sAddress = sanityFunction(address);
+    if (!sFullName || !sTckn || !sPhone || !sAddress) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL addServiceSP (?,?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sFullName,
+      sTckn,
+      sPhone,
+      sAddress,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
   getAllSP,
+  addDataSP,
 };
