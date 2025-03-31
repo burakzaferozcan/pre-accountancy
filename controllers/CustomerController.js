e8b44d98218926cfb794e950263d3870357cad81
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:28:38 +0300

    feat: Implement getByIdSP function in CustomerController to retrieve customer details by ID with error handling

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -39,7 +39,24 @@ const addDataSP = async (req, res) => {
   }
 };
 
-const getByIdSP = async (req, res) => {};
+const getByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL getCustomerByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 const updateByIdSP = async (req, res) => {};
 const deleteByIdSP = async (req, res) => {};
 
