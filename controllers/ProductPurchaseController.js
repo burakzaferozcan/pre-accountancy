578eba8ae61ee028fe28d6a5c9031667eefa556d
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 23:15:46 +0300

    feat: Add addPaymentSP function in ProductPurchaseController for processing payments with input validation and error handling

--- controllers/ProductPurchaseController.js
+++ controllers/ProductPurchaseController.js
@@ -48,7 +48,33 @@ const deleteProductPurchaseSP = async (req, res) => {
   }
 };
 
+const addPaymentSP = async (req, res) => {
+  try {
+    let { serviceID, description, total } = req.body;
+    const sServiceID = sanityFunction(serviceID);
+    const sDescription = sanityFunction(description);
+    const sTotal = sanityFunction(total);
+    if (!sServiceID || !sDescription || !sTotal) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL addPaymentSP (?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sServiceID,
+      sDescription,
+      sTotal,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   addProductPurchaseSP,
   deleteProductPurchaseSP,
+  addPaymentSP,
 };
