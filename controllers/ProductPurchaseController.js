a4fff637ec04feb0f76ff4c09d9943496d7601a5
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 23:13:09 +0300

    feat: Add deleteProductPurchaseSP function in ProductPurchaseController for deleting product purchases by ID with input validation and error handling

--- controllers/ProductPurchaseController.js
+++ controllers/ProductPurchaseController.js
@@ -29,6 +29,26 @@ const addProductPurchaseSP = async (req, res) => {
   }
 };
 
+const deleteProductPurchaseSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL deleteProductPurchaseByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   addProductPurchaseSP,
+  deleteProductPurchaseSP,
 };
