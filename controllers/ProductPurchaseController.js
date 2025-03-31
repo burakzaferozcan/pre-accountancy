9a7a903db3feb091777098a76ece862188c5f3c5
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 23:12:12 +0300

    feat: Add addProductPurchaseSP function in ProductPurchaseController for processing product purchases with input validation and error handling

---
+++ controllers/ProductPurchaseController.js
@@ -0,0 +1,34 @@
+const { getConnection } = require("../config/dbConfig");
+const { sanityFunction } = require("../utils/sanityFunction");
+
+const addProductPurchaseSP = async (req, res) => {
+  try {
+    let { serviceID, stockID, amount, price } = req.body;
+    const sServiceID = sanityFunction(serviceID);
+    const sStockID = sanityFunction(stockID);
+    const sAmount = sanityFunction(amount);
+    const sPrice = sanityFunction(price);
+
+    if (!sServiceID || !sStockID || !sAmount || !sPrice) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL addProductPurchaseSP (?,?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sServiceID,
+      sStockID,
+      sAmount,
+      sPrice,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  addProductPurchaseSP,
+};
