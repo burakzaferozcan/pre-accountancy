e43473f097c67718ada64831eb37622321048a29
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 23:24:01 +0300

    feat: Add getAllPaymentByIdSP function in ProductPurchaseController for retrieving payments by ID with error handling

--- controllers/ProductPurchaseController.js
+++ controllers/ProductPurchaseController.js
@@ -111,9 +111,30 @@ const getAllProductPurchaseByIdSP = async (req, res) => {
   }
 };
 
+const getAllPaymentByIdSP = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL getAllPaymentByIdSP (?)";
+    const response = await connection.query(sqlQuery);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   addProductPurchaseSP,
   deleteProductPurchaseSP,
   addPaymentSP,
   deletePaymentSP,
+  getAllProductPurchaseByIdSP,
+  getAllPaymentByIdSP,
 };
