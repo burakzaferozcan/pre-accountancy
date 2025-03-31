1105e118ba62eff082aace02b3cf5e1636b39716
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-03-31 22:25:39 +0300

    feat: Implement addDataSP function in CustomerController to add new customers with input validation

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,15 +1,44 @@
 const { getConnection } = require("../config/dbConfig");
+const { sanityFunction } = require("../utils/sanityFunction");
 const getAllSP = async (req, res) => {
   try {
     const connection = await getConnection();
     const sqlQuery = "CALL getAllCustomerSP ()";
     const response = await connection.query(sqlQuery);
     res.send(response[0][0].result);
-  } catch (e) {
-    res.status(500).send(e.message);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 };
-const addDataSP = async (req, res) => {};
+const addDataSP = async (req, res) => {
+  try {
+    let { fullName, tckn, phone, address, reference } = req.body;
+    const sFullName = sanityFunction(fullName);
+    const sTckn = sanityFunction(tckn);
+    const sPhone = sanityFunction(phone);
+    const sAddress = sanityFunction(address);
+    const sReference = sanityFunction(reference);
+    if (!sFullName || !sPhone || !sAddress || !sTckn || !sReference) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL addCustomerSP (?,?,?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sFullName,
+      sPhone,
+      sAddress,
+      sTckn,
+      sReference,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 const getByIdSP = async (req, res) => {};
 const updateByIdSP = async (req, res) => {};
 const deleteByIdSP = async (req, res) => {};
