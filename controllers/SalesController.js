086a64a0eaf206a783db9927c772e87032eea0c1
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-04-01 12:38:18 +0300

    feat: Add addSalesSP function in SalesController for processing sales with input validation and error handling

---
+++ controllers/SalesController.js
@@ -0,0 +1,34 @@
+const { getConnection } = require("../config/dbConfig");
+const { sanityFunction } = require("../utils/sanityFunction");
+
+const addSalesSP = async (req, res) => {
+  try {
+    let { customerID, stockID, amount, price } = req.body;
+    const sCustomerID = sanityFunction(customerID);
+    const sStockID = sanityFunction(stockID);
+    const sAmount = sanityFunction(amount);
+    const sPrice = sanityFunction(price);
+
+    if (!sCustomerID || !sStockID || !sAmount || !sPrice) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL addSalesSP (?,?,?,?)";
+    const response = await connection.query(sqlQuery, [
+      sCustomerID,
+      sStockID,
+      sAmount,
+      sPrice,
+    ]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  addSalesSP,
+};
