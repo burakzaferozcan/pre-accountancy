8c05b877992fcff0c598b60f965573928e6acc21
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-04-06 22:08:16 +0300

    feat: Add user login endpoint with input validation and password comparison in AuthRoutes

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const { getConnection } = require("../config/dbConfig");
@@ -58,3 +58,60 @@ router.post(
     }
   }
 );
+
+router.post(
+  "/login",
+  limiter,
+  [
+    check("userName", "Lütfen bir kullanıcı adı girniz").not().isEmpty(),
+    check("userPassword", "Şifre en az 6 karakter olmalı").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { userName, userPassword } = req.body;
+      const connection = await getConnection();
+
+      const sqlQuery = "CALL userLoginSP (?,?)";
+      const existingUser = await connection.query(sqlQuery, [
+        userName,
+        userPassword,
+      ]);
+      if (existingUser.length === 0) {
+        return res.status(400).json({
+          success: false,
+          message: "Kullanıcı adı veya şifre yanlış.",
+        });
+      }
+
+      const currentPassword = existingUser[0][0].userPassword;
+      const isMatch = await bcrypt.compare(userPassword, currentPassword);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          isLogin: false,
+          message: "Kullanıcı adı veya şifre yanlış.",
+        });
+      }
+
+      const payload = { user: response[0][0].userName };
+      jwt.sign(payload, JWTSECRET, { expiresIn: "12h" }, (err, token) => {
+        if (err) throw err;
+        res.json({ token, userName, userID: existingUser[0][0].userID });
+      });
+      res.send(response[0][0].result);
+    } catch (error) {
+      res.status(500).json({
+        message: "Sunucu hatası",
+        isRegistered: false,
+      });
+    }
+  }
+);
+
+module.exports = {
+  authRoutes: router,
+};
