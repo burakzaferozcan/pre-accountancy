6a03aafb1e316d9240b6e6af784220b6c4cefa50
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-10 15:16:40 +0300

    feat: implement authentication slice with user registration logic

---
+++ client/src/slices/auth/AuthSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const user = JSON.parse(localStorage.getItem("auth"));
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (data, thunkAPI) => {
+    try {
+      return await authService.register(data);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
