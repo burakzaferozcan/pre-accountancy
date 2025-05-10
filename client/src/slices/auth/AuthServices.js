800350520d1ec6062d974fc46ca6feab8e68ca36
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-10 15:18:56 +0300

    feat: add login function to AuthServices for user authentication

--- client/src/slices/auth/AuthServices.js
+++ client/src/slices/auth/AuthServices.js
@@ -9,8 +9,17 @@ const register = async (data) => {
   return response.data;
 };
 
+const login = async (data) => {
+  const response = await axios.post(BASE_URL + "/login", data);
+  if (response.data) {
+    localStorage.setItem("auth", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 const authService = {
   register,
+  login,
 };
 
 export default authService;
