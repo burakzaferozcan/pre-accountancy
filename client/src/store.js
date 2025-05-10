d59afa43a69032b67bd737ade8545cef5cea2008
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-10 15:44:16 +0300

    fix: correct import statement for authReducer in store configuration

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "./slices/auth/AuthSlice";
+import authReducer from "./slices/auth/AuthSlice";
+
 export const store = configureStore({
   reducer: { auth: authReducer },
 });
