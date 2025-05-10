9cf37db2e83505093e33c8a08166bc00f9c9e964
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-10 15:27:54 +0300

    feat: add reset reducer and update extraReducers in AuthSlice

--- client/src/slices/auth/AuthSlice.js
+++ client/src/slices/auth/AuthSlice.js
@@ -43,3 +43,33 @@ export const login = createAsyncThunk("auth/login", async (data, thunkAPI) => {
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
+});
