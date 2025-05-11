b417ffac875669bb3ea6a5228322379d13c87d6a
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-11 18:43:45 +0300

    feat: add customer reducer to the store configuration

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth/AuthSlice";
+import customerReducer from "./slices/customer/CustomerSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, customer: customerReducer },
 });
