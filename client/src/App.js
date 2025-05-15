26c6b43bb73c58f5cc17219f59afe6d140f2b55c
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-15 22:02:27 +0300

    feat: implement user authentication check and redirect to login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "./components/Partials/Header";
 import Footer from "./components/Partials/Footer";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (!(user && user.token)) {
+      navigate("/login");
+    }
+  }, [navigate, user]);
   return (
     <>
       <Header />
