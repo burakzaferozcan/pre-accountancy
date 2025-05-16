4bfe7860548cc164b724fae25cd668e0f1ade857
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-16 16:14:45 +0300

    feat: implement session expiration check and logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,31 @@ import "./App.css";
 import { Outlet, useNavigate } from "react-router-dom";
 import Header from "./components/Partials/Header";
 import Footer from "./components/Partials/Footer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "./slices/auth/AuthSlice";
 
 function App() {
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   React.useEffect(() => {
     if (!(user && user.token)) {
       navigate("/login");
     }
   }, [navigate, user]);
+
+  React.useEffect(() => {
+    const expiretionTime = localStorage.getItem("expireTime");
+    if (expiretionTime) {
+      const currentTime = new Date().getTime();
+      if (expiretionTime < currentTime) {
+        dispatch(logout);
+        navigate("/login");
+      }
+    }
+  }, [navigate]);
+
   return (
     <>
       <Header />
