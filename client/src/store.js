56ad666eadda09973b04b752955589f5485c098f
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-17 21:23:23 +0300

    feat: add sales reducer to the store configuration

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth/AuthSlice";
 import customerReducer from "./slices/customer/CustomerSlice";
+import salesReducer from "./slices/sales/SalesSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer, customer: customerReducer },
+  reducer: {
+    auth: authReducer,
+    customer: customerReducer,
+    sales: salesReducer,
+  },
 });
