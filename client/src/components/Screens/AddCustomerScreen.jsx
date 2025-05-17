b89364d62792e8d03c39892a144060cc5caabbca
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-17 20:03:32 +0300

    feat: implement customer addition form with input handling and save button

--- client/src/components/Screens/AddCustomerScreen.jsx
+++ client/src/components/Screens/AddCustomerScreen.jsx
@@ -1,7 +1,99 @@
 import React from "react";
+import { FaSave } from "react-icons/fa";
 
 function AddCustomerScreen() {
-  return <div>AddCustomerScreen</div>;
+  const [data, setData] = React.useState({
+    fullName: "",
+    tckn: "",
+    phone: "",
+    address: "",
+    reference: "",
+  });
+
+  const { fullName, tckn, phone, address, reference } = data;
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container mt-4">
+      <h3 className="text-center mb-2">Müşteri Ekleme</h3>
+      <hr className="my-2" />
+      <div className="d-flex flex-column gap-2">
+        <div className="d-flex flex-row gap-1">
+          <label htmlFor="fullName" className="form-label col-4">
+            Adı Soyadı
+          </label>
+          <input
+            type="text"
+            name="fullName"
+            placeholder="Adı Soyadı"
+            onChange={onChange}
+            className="form-control"
+            value={fullName}
+          />
+        </div>
+
+        <div className="d-flex flex-row gap-1">
+          <label htmlFor="tckn" className="form-label col-4">
+            TC Kimlik No
+          </label>
+          <input
+            type="text"
+            name="tckn"
+            placeholder="TC Kimlik NO"
+            onChange={onChange}
+            className="form-control"
+            value={tckn}
+          />
+        </div>
+        <div className="d-flex flex-row gap-1">
+          <label htmlFor="phone" className="form-label col-4">
+            Telefon
+          </label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="Telefon"
+            onChange={onChange}
+            className="form-control"
+            value={phone}
+          />
+        </div>
+        <div className="d-flex flex-row gap-1">
+          <label htmlFor="address" className="form-label col-4">
+            Adres
+          </label>
+          <input
+            type="text"
+            name="address"
+            placeholder="Adres"
+            onChange={onChange}
+            className="form-control"
+            value={address}
+          />
+        </div>
+        <div className="d-flex flex-row gap-1">
+          <label htmlFor="reference" className="form-label col-4">
+            Referans
+          </label>
+          <input
+            type="text"
+            name="reference"
+            placeholder="Adres"
+            onChange={onChange}
+            className="form-control"
+            value={reference}
+          />
+        </div>
+      </div>
+      <button onClick={customerSave} className="btn btn-primary mt-2 w-100">
+        <FaSave />
+        Kaydet
+      </button>
+    </div>
+  );
 }
 
 export default AddCustomerScreen;
