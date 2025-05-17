665c2ea260077b614652302eda3c6cc2f9b087e2
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-17 21:06:30 +0300

    feat: enhance CustomerSalesScreen layout with structured components

--- client/src/components/Screens/CustomerSalesScreen.jsx
+++ client/src/components/Screens/CustomerSalesScreen.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function CustomerSalesScreen() {
-  return <div>CustomerSalesScreen</div>;
+  return (
+    <div className="home-container d-flex flex-column gap-2">
+      <div className="names  d-flex flex-row gap-2">
+        <div className="col-8">
+          <CustomerSalesProcess />
+        </div>
+        <div className="col-4">
+          <CustomerCollectionProcess />
+        </div>
+      </div>
+      <div className="tables d-flex flex-row gap-2">
+        <div className="col-8">
+          <CustomerSalesTable />
+        </div>
+        <div className="col-4 d-flex flex-column gap-2">
+          <CustomerCollectionTable />
+          <CustomerBalance />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CustomerSalesScreen;
