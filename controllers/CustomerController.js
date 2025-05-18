4ef7e0ec855bb37c9c5fd5c20d2c9b7ba50c38db
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-18 17:34:35 +0300

    feat: implement getCustomerInfoById function to fetch customer details by ID

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -106,10 +106,30 @@ const deleteByIdSP = async (req, res) => {
   }
 };
 
+const getCustomerInfoById = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const sId = sanityFunction(id);
+    if (!sId) {
+      res.send({
+        success: false,
+        message: "Lütfen geçerli veriler ile tekrar deneyin.",
+      });
+    }
+    const connection = await getConnection();
+    const sqlQuery = "CALL getCustomerInfoById (?)";
+    const response = await connection.query(sqlQuery, [sId]);
+    res.send(response[0][0].result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
   getAllSP,
   addDataSP,
   getByIdSP,
   updateByIdSP,
   deleteByIdSP,
+  getCustomerInfoById,
 };
