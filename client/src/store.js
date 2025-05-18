9c178b29e53c6c1c378e6cccc189ebfe3a3d5279
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-18 10:28:11 +0300

    feat: add stock reducer to the store configuration

--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import authReducer from "./slices/auth/AuthSlice";
 import customerReducer from "./slices/customer/CustomerSlice";
 import salesReducer from "./slices/sales/SalesSlice";
 import collectionsReducer from "./slices/collections/CollectionsSlice";
+import stockReducer from "./slices/stock/stockSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     customer: customerReducer,
     sales: salesReducer,
     collections: collectionsReducer,
+    stock: stockReducer,
   },
 });
