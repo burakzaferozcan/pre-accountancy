627d5df1986838b73c9a7d9427de8b960ae6c3ff
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-18 18:08:54 +0300

    feat: implement CustomerCollectionProcess component with form for adding collections

--- client/src/components/CustomerCollectionProcess.jsx
+++ client/src/components/CustomerCollectionProcess.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { FaSave } from "react-icons/fa";
+import { addCollections } from "../../slices/collections/CollectionsSlice";
+import { setBalanceRefresh } from "../slices/customer/CustomerSlice";
 
 function CustomerCollectionProcess() {
-  return <div>CustomerCollectionProcess</div>;
+  const params = useParams();
+  const customerID = params.id;
+  const dispatch = useDispatch();
+  const [data, setData] = React.useState({
+    custmerID: customerID,
+    description: "",
+    price: 0,
+  });
+
+  const { description, price } = data;
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const collectionSave = async () => {
+    dispatch(addCollections(data));
+    dispatch(setBalanceRefresh());
+  };
+
+  return (
+    <div className="mt-2 d-flex flex-column gap-1" style={{ width: "97%" }}>
+      <h4 className="text-center">Müşteri Tahsilat</h4>
+      <hr className="my-2" />
+      <div className="d-flex flex-row gap-2">
+        <label htmlFor="description" className="col-3">
+          Açıklama
+        </label>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={onChange}
+          className="form-control mt-1"
+        />
+      </div>
+      <div className="d-flex flex-row gap-2">
+        <label htmlFor="price" className="col-3">
+          Tutar
+        </label>
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={onChange}
+          className="form-control mt-1"
+        />
+      </div>
+      <button className="btn btn-primary" onClick={collectionSave}>
+        <FaSave /> Kaydet
+      </button>
+    </div>
+  );
 }
 
 export default CustomerCollectionProcess;
