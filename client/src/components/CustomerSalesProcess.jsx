5e36ac730ffc4fe74d414830b74e4013b1600d11
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-18 11:00:46 +0300

    feat: enhance CustomerSalesProcess with stock data input and filtering functionality

--- client/src/components/CustomerSalesProcess.jsx
+++ client/src/components/CustomerSalesProcess.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllStock } from "../slices/stock/stockSlice";
-
+import { useParams } from "react-router-dom";
+import { FaSave } from "react-icons/fa";
 function CustomerSalesProcess() {
   const dispatch = useDispatch();
+  const params = useParams();
+  const customerID = params.id;
   const { stockTable, isLoading, isSuccess, message } = useSelector(
     (state) => state.stock
   );
+  const [data, setData] = React.useState({
+    customerID: customerID,
+    stockID: "",
+    description: "",
+    amount: 0,
+    price: 0,
+  });
+  const { stockID, description, amount, price } = data;
+  const priceRef = React.createRef();
+  const amountRef = React.createRef();
+  const saveRef = React.createRef();
 
   React.useEffect(() => {
     if (!isSuccess || isLoading) {
@@ -14,9 +28,73 @@ function CustomerSalesProcess() {
     }
   }, [isLoading, isSuccess]);
 
+  const onStockChange = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const filterStockData = description
+    ? stockTable.filter((item) =>
+        item.description.toLowerCase().includes(description.toLowerCase())
+      )
+    : stockTable;
+
   return (
-    <div className="d-flex flex-row gap-1 mt-2">
-      <div className="col-7"></div>
+    <div
+      className="d-flex flex-row gap-1 mt-2"
+      style={{
+        marginLeft: "10px",
+      }}
+    >
+      <div className="col-7 d-flex flex-column gap-3">
+        <div className="input-group">
+          <label htmlFor="description" className="col-4">
+            Stok Adı :
+          </label>
+          <input
+            type="text"
+            name="description"
+            onChange={onStockChange}
+            value={description}
+            className="form-conrol"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="price" className="col-4">
+            Fiyat :
+          </label>
+          <input
+            ref={priceRef}
+            type="text"
+            name="price"
+            onChange={onChange}
+            value={price}
+            className="form-conrol"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="amount" className="col-4">
+            Miktar :
+          </label>
+          <input
+            ref={amountRef}
+            type="text"
+            name="amount"
+            onChange={onChange}
+            value={amount}
+            className="form-conrol"
+          />
+        </div>
+        <button ref={saveRef} className="btn btn-primary">
+          <FaSave /> Satış Kaydet
+        </button>
+      </div>
       <div className="col-5" style={{ overflow: "auto", height: "200px" }}>
         <div className="table-responsive">
           <table className="table">
@@ -27,9 +105,9 @@ function CustomerSalesProcess() {
                 <th>Miktar</th>
               </tr>
             </thead>
-            {stockTable && stockTable.length > 0 && (
+            {filterStockData && filterStockData.length > 0 && (
               <tbody>
-                {stockTable.map((item) => (
+                {filterStockData.map((item) => (
                   <tr key={item.id}>
                     <td>{item.description}</td>
                     <td>{item.price}</td>
