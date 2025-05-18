45ec9b60f9f815c4d6ae3fc1fb1ee667fea5704b
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-18 12:14:14 +0300

    feat: implement sales table display in CustomerSalesTable component

--- client/src/components/CustomerSalesTable.jsx
+++ client/src/components/CustomerSalesTable.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 function CustomerSalesTable() {
-  return <div>CustomerSalesTable</div>;
+  const { salesTable, isUpdate } = useSelector((state) => state.sales);
+  return (
+    <div
+      className="table-responsive"
+      style={{ marginLeft: "10px", overflowX: "auto" }}
+    >
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>#</th>
+            <th>Tarih</th>
+            <th>Acaklama</th>
+            <th>Fiyat</th>
+            <th>Miktar</th>
+            <th>Toplam</th>
+          </tr>
+        </thead>
+        {salesTable && salesTable.length > 0 && (
+          <tbody>
+            {salesTable.map((sale) => (
+              <tr key={sale.id}>
+                <td>{sale.id}</td>
+                <td>{sale.date}</td>
+                <td>{sale.description}</td>
+                <td>{sale.price}</td>
+                <td>{sale.amount}</td>
+                <td>{sale.total}</td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </table>
+    </div>
+  );
 }
 
 export default CustomerSalesTable;
