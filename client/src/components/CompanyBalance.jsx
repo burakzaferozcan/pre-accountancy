758bbdbc6e018eab043eb22466d7f1947c07c900
burak zafer ozcan <ozcanburakzafer@gmail.com>
2025-05-19 16:37:43 +0300

    feat: implement CompanyBalance component with balance display and data fetching

--- client/src/components/CompanyBalance.jsx
+++ client/src/components/CompanyBalance.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCompanyInfoById } from "../slices/company/CompanySlice";
+import { useParams } from "react-router-dom";
 
 function CompanyBalance() {
-  return <div>CompanyBalance</div>;
+  const dispatch = useDispatch();
+  const params = useParams();
+  const companyID = params.id;
+  const { balanceTable, isBalanceRefresh } = useSelector(
+    (state) => state.company
+  );
+
+  React.useEffect(() => {
+    if (isBalanceRefresh) {
+      dispatch(getCompanyInfoById(companyID));
+    }
+  }, [isBalanceRefresh, companyID]);
+
+  return (
+    <div className="card mt-4" style={{ padding: "5px", width: "97%" }}>
+      <div className="d-flex flex-column">
+        <div className="d-flex col-12 gap-1">
+          <div className="d-flex-col-6">
+            <label htmlFor="debts">
+              Alacak :{" "}
+              <strong>
+                {Number(balanceTable.totalDebts).toLocaleString("tr-TR", {
+                  style: "currency",
+                  currency: "TRY",
+                })}
+              </strong>
+            </label>
+            <label htmlFor="collections">
+              Ödeme :{" "}
+              <strong>
+                {Number(balanceTable.totalCollections).toLocaleString("tr-TR", {
+                  style: "currency",
+                  currency: "TRY",
+                })}
+              </strong>
+            </label>
+          </div>
+        </div>
+        <div className="d-flex col-12 gap-1">
+          <div className="d-flex-col-6"></div>
+          <div className="d-flex-col-6">
+            <label htmlFor="balance">
+              Bakiye :{" "}
+              <strong>
+                {Number(balanceTable.balance).toLocaleString("tr-TR", {
+                  style: "currency",
+                  currency: "TRY",
+                })}
+              </strong>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CompanyBalance;
